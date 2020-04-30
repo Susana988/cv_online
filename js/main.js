692ac17a1b44eb354f6849043d900d92831d81d7
Susana <susanaaorfao@gmail.com>
2020-04-30 11:19:47 +0100

    First function Jquery created
    Bars fill jquery created.
    New content added.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 (function ($) {
     $(window).on("load", function () {
         $(".site-content-page-wrap").mCustomScrollbar({ theme: "minimal" });
+        var value = $(".skills-item").data("value");
+        $(".skills-line-fill").css("width", value);
     });
 })(jQuery);
